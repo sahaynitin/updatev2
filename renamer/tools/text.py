5917f93b6ba437995f1cd5fef7d9f54869b846e5
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-19 13:32:43 +0530

    Create text.py

---
+++ renamer/tools/text.py
@@ -0,0 +1,44 @@
+from pyrogram.emoji import *
+
+
+class TEXT:
+    DOWNLOAD_START = "Please Wait I'm Trying To Download 😇"
+    UPLOAD_START = "Please Wait .I'm Ready To Upload🔥"
+    UPLOAD_SUCESS = "Thanks for using Me. \n Update Channel @TMWAD 🥳"
+    BANNED_USER_TEXT = "Hey bro, you are **banned** from using me.😡"
+    NOT_LOGGED_TEXT = "This bot was only for private use. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = "Thumbnail Saved Permanently ✅"
+    DELETED_CUSTOM_THUMBNAIL = "Thumbnail Deleted Successfully ✅"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "𝖭𝗈 𝗍𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 🐙"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
+
+    ABOUT = """
+
+☞ My Name : Renamer Bot
+
+☞ Updates : @TMWAD    
+
+☞ Language : Python3
+
+☞ <b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a>
+
+"""
+
+    HELP_USER = """**Follow Below Steps:**
+   
+☞︎︎︎ Use /mode command to change upload mode.
+☞︎︎︎ Send a photo to set as permanent thumbnail.
+☞︎︎︎ Now send me the Telegram file you want to rename.
+☞︎︎︎ Send the new name when bot ask.
+
+**Increase Speed**
+
+☞︎︎︎ Use Normal Font Style As File Name
+☞︎︎︎ After File Name Give File category(Mkv/Mp4/..etc)
+☞︎︎︎ Wait Untill Previous Process succsus
+"""
+
+    START_TEXT = """Hi {user_mention},
+
+I am a telegram renamer Bot. Use /
+"""
