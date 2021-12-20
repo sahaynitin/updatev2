f32c9b678ae64df07312a96e8afe1378dcc8e2be
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-20 16:04:08 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -2,8 +2,18 @@ import os
 import logging
 logger = logging.getLogger(__name__)
 
-
 class Config:
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Video-Merge-Bot")
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL")
+    LOG_CHANNEL = os.environ.get("LOG_CHANNEL")
+    DOWN_PATH = os.environ.get("DOWN_PATH", "./downloads")
+    TIME_GAP = int(os.environ.get("TIME_GAP", 5))
+    MAX_VIDEOS = int(os.environ.get("MAX_VIDEOS", 5))
+    STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
+    STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
+    MONGODB_URI = os.environ.get("MONGODB_URI")
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", False))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER", 1445283714))
     API_ID = int(os.environ.get("API_ID", 12345))
     API_HASH = os.environ.get("API_HASH")
     OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
